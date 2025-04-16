e888e57d30d93f96e97074062feb69db58f6e447
A-M98 <aalmohammad@pifss.gov.kw>
2025-04-16 11:29:56 +0300

    Ahmed Mohammed && Abdullah Al-Kharji

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,52 +1,72 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Product } from '../../data/products';
 
 @Injectable({ providedIn: 'root' })
 export class CartService {
-  private cartItems: (Product & { quantity: number })[] = [];
+  private cartSubject = new BehaviorSubject<(Product & { quantity: number })[]>(
+    []
+  );
+  cart$ = this.cartSubject.asObservable();
+
+  constructor() {}
 
   addToCart(product: Product) {
-    const item = this.cartItems.find((p) => p.id === product.id);
+    const currentCart = this.cartSubject.getValue();
+    const item = currentCart.find((p) => p.id === product.id);
+
     if (item) {
       if (item.quantity < item.stock) {
         item.quantity++;
       }
     } else {
-      this.cartItems.push({ ...product, quantity: 1 });
+      currentCart.push({ ...product, quantity: 1 });
     }
+
+    this.cartSubject.next([...currentCart]);
   }
 
   getCart() {
-    return this.cartItems;
+    return this.cartSubject.getValue();
   }
 
   incrementQuantity(productId: number) {
-    const item = this.cartItems.find((p) => p.id === productId);
+    const currentCart = this.cartSubject.getValue();
+    const item = currentCart.find((p) => p.id === productId);
+
     if (item && item.quantity < item.stock) {
       item.quantity++;
+      this.cartSubject.next([...currentCart]);
     }
   }
 
   decrementQuantity(productId: number) {
-    const item = this.cartItems.find((p) => p.id === productId);
+    const currentCart = this.cartSubject.getValue();
+    const item = currentCart.find((p) => p.id === productId);
+
     if (item) {
       item.quantity--;
       if (item.quantity <= 0) {
         this.removeFromCart(productId);
+      } else {
+        this.cartSubject.next([...currentCart]);
       }
     }
   }
 
   removeFromCart(productId: number) {
-    this.cartItems = this.cartItems.filter((p) => p.id !== productId);
+    const currentCart = this.cartSubject.getValue();
+    const updatedCart = currentCart.filter((p) => p.id !== productId);
+    this.cartSubject.next(updatedCart);
   }
 
   clearCart() {
-    this.cartItems = [];
+    this.cartSubject.next([]);
   }
 
   getTotal() {
-    return this.cartItems.reduce(
+    const currentCart = this.cartSubject.getValue();
+    return currentCart.reduce(
       (total, item) => total + item.price * item.quantity,
       0
     );
