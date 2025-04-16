11c8602dd9735759f0374d9fca1d0f63f372ae73
hsnAlwayel <hsn.alwayel@gmail.com>
2025-04-16 11:08:42 +0300

    Hasan Alwayel

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,52 +1,55 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../../data/products';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class CartService {
-  private cartItems: (Product & { quantity: number })[] = [];
+  cartItems = new BehaviorSubject<(Product & { quantity: number })[]>([]);
 
   addToCart(product: Product) {
-    const item = this.cartItems.find((p) => p.id === product.id);
+    const item = this.cartItems.value.find((p) => p.id === product.id);
     if (item) {
       if (item.quantity < item.stock) {
         item.quantity++;
+        this.cartItems.next(this.cartItems.value);
       }
     } else {
-      this.cartItems.push({ ...product, quantity: 1 });
+      this.cartItems.next([
+        ...this.cartItems.value,
+        { ...product, quantity: 1 },
+      ]);
     }
   }
 
-  getCart() {
-    return this.cartItems;
-  }
-
   incrementQuantity(productId: number) {
-    const item = this.cartItems.find((p) => p.id === productId);
+    const item = this.cartItems.value.find((p) => p.id === productId);
     if (item && item.quantity < item.stock) {
       item.quantity++;
+      this.cartItems.next(this.cartItems.value);
     }
   }
 
   decrementQuantity(productId: number) {
-    const item = this.cartItems.find((p) => p.id === productId);
+    const item = this.cartItems.value.find((p) => p.id === productId);
     if (item) {
       item.quantity--;
       if (item.quantity <= 0) {
         this.removeFromCart(productId);
       }
+      this.cartItems.next(this.cartItems.value);
     }
   }
 
   removeFromCart(productId: number) {
-    this.cartItems = this.cartItems.filter((p) => p.id !== productId);
+    this.cartItems.next(this.cartItems.value.filter((p) => p.id !== productId));
   }
 
   clearCart() {
-    this.cartItems = [];
+    this.cartItems.next([]);
   }
 
   getTotal() {
-    return this.cartItems.reduce(
+    return this.cartItems.value.reduce(
       (total, item) => total + item.price * item.quantity,
       0
     );
