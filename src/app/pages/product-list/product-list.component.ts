e238189d65b82b85791a953bfe4eca4e0fbca2d9
MishariAldoub <MEAldoub@pifss.gov.kw>
2025-04-17 10:59:39 +0300

    test push

--- src/app/pages/product-list/product-list.component.ts
+++ src/app/pages/product-list/product-list.component.ts
@@ -1,14 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { PRODUCTS } from '../../../data/products';
 import { ProductCardComponent } from '../../components/product-card/product-card.component';
+import { count } from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
   imports: [ProductCardComponent],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
   products = PRODUCTS;
+  counter = signal(0);
+  doubleCounter = computed(() => this.counter() * 2);
+  multiply = signal(2);
+
+  constructor() {
+    effect(() => {
+      console.log('yert: ', this.counter());
+    });
+  }
+
+  logEffect = effect(() => {
+    console.log('Counter chamhed to', this.counter());
+  });
+
+  resetCount() {
+    this.counter.set(0);
+  }
+  setCount() {
+    this.counter.set(10);
+  }
+  inc() {
+    this.counter.update((val) => val + 1);
+    // this.counter.update(this.counter + 1);
+  }
+  dec() {
+    this.counter.update((val) => val - 1);
+  }
+  isMultiply() {
+    this.multiply.update((val) => val * 2);
+  }
 }
